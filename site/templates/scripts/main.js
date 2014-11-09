bc6f7dbdc87ec6bca28e4f488171e6f76825c537
Fred Lieutaud <flieutaud@gmail.com>
2014-11-09 11:38:52 +0100

    Add participation reports Add Team report adjustements + dependencies
    Add bootstrap dependency
    Add jquery dependency

--- site/templates/scripts/main.js
+++ site/templates/scripts/main.js
@@ -18,4 +18,44 @@ $(document).ready(function() {
 
     return false; 
   });
+
+
+  $('#participation').click( function() {
+    var limitCheckbox = $('#limit10');
+    if ($(this).is(':checked')) {
+      limitCheckbox.removeAttr('disabled'); // Enable limit
+      $('a.reportButton').each( function() {
+        var href = $(this).attr('href');
+        $(this).attr('href', href+'/participation');
+      });
+    } else {
+      $('a.reportButton').each( function() {
+        var href = $(this).attr('href');
+        if (href.indexOf('/participation') != -1) {
+          var mySplitResult = href.split("\/");
+          if (limitCheckbox.is(':checked')) { // Uncheck limit
+            href = href.substring(0, href.length-3);
+          }
+          href = href.substring(0, href.length-14); // remove '/participation' from url
+          $(this).attr('href', href); // remove '/participation' from url
+        }
+      });
+      limitCheckbox.attr('checked', false);
+      limitCheckbox.attr('disabled', true); // Disabled limit
+    }
+  });
+
+  $('#limit10').click( function() {
+    if ($(this).is(':checked')) {
+      $('a.reportButton').each( function() {
+        var href = $(this).attr('href');
+        $(this).attr('href', href+'/10');
+      });
+    } else {
+      $('a.reportButton').each( function() {
+        var href = $(this).attr('href');
+        $(this).attr('href', href.substring(0, href.length-3));
+      });
+    }
+  });
 }); 
