8bbea7d43c74fde907638154683851767aeef068
Fred Lieutaud <flieutaud@gmail.com>
2017-10-04 11:15:25 +0200

    Redirect to login form when logging out

--- site/templates/_init.php
+++ site/templates/_init.php
@@ -3,6 +3,7 @@
   include('./my-functions.inc'); // Planet Alert PHP functions
 
   $homepage = $pages->get("/"); 
+  $loginUrl = $pages->get("name=loginform")->url;
 
   $wire->addHook('LazyCron::everyDay', null, 'checkActivity'); // Check all players activity once a day
 
