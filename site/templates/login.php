e5786ae7d6e22c643555f129e169a787244cb17f
Fred Lieutaud <flieutaud@gmail.com>
2018-06-24 15:14:46 +0200

    Fixed login form if user is already logged in. The form is no longer displayed.

--- site/templates/login.php
+++ site/templates/login.php
@@ -32,22 +32,27 @@ $logoUrl = $pages->get("name=home")->photo->eq(1)->url;
 <div class="row">
   <div class="col-md-10 text-center">
     <h1>Welcome to Planet Alert !</h1>
-    <?php if($input->post->username) echo "<h3><span class='label label-danger'>Login failed... (check user name or password)</span></h3>"; ?>
-    <form class="form-horizontal loginForm" action="<?php echo $page->url; ?>?sender=<?php echo $input->get('sender').'&team='.$input->get('team'); ?>" method="post">
-      <div class="form-group">
-        <label for="username" class="col-sm-4 control-label">User :</label>
-        <div class="col-sm-6">
-          <input class="form-control" type="text" name="username" id="username" placeholder="Username" />
+    <?php if (!$user->isLoggedin()) {
+      if($input->post->username) echo "<h3><span class='label label-danger'>Login failed... (check user name or password)</span></h3>"; ?>
+      <form class="form-horizontal loginForm" action="<?php echo $page->url; ?>?sender=<?php echo $input->get('sender').'&team='.$input->get('team'); ?>" method="post">
+        <div class="form-group">
+          <label for="username" class="col-sm-4 control-label">User :</label>
+          <div class="col-sm-6">
+            <input class="form-control" type="text" name="username" id="username" placeholder="Username" />
+          </div>
         </div>
-      </div>
-      <div class="form-group">
-        <label for="pass" class="col-sm-4 control-label">Password :</label>
-        <div class="col-sm-6">
-          <input class="form-control" type="password" name="pass" id="pass" placeholder="Password" /></label></p>
+        <div class="form-group">
+          <label for="pass" class="col-sm-4 control-label">Password :</label>
+          <div class="col-sm-6">
+            <input class="form-control" type="password" name="pass" id="pass" placeholder="Password" /></label></p>
+          </div>
         </div>
-      </div>
-      <input type='submit' class="btn btn-info" name='submit' value='Connect' />
-    </form>
+        <input type='submit' class="btn btn-info" name='submit' value='Connect' />
+      </form>
+    <?php } else { ?>
+      <div class="well">You are already logged in. Have fun using Planet Alert ;)</div>
+      <p>Log out in the menu above if needed.</p>
+    <?php } ?>
   </div>
 
   <div class="col-md-2">
