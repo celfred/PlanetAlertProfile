b7198d830c0ac88dd99aad858f90d9ab6050fefb
Fred Lieutaud <flieutaud@gmail.com>
2018-09-21 20:24:27 +0200

    Fix training monsters list limited to logged teacher Fix imagemap exercise type

--- site/templates/training.php
+++ site/templates/training.php
@@ -13,9 +13,13 @@
       $out = '<div>';
       if (!$input->get->id) { // Display training catalogue
         // Set all available monsters
-        if ($user->isSuperuser() || $user->hasRole('teacher')) {
+        if ($user->isSuperuser()) {
           $allMonsters = $pages->find('template=exercise, sort=name, include=all');
-        } else {
+        }
+        if ($user->hasRole('teacher')) {
+          $allMonsters = $pages->find("template=exercise, (created_users_id=$user->id), (teacher=$user), include=all")->sort("name");
+        }
+        if ($user->hasRole('player')) {
           // Check if player has the Visualizer (or forced by admin)
           if ($player->equipment->has("name~=visualizer") || $player->team->forceVisualizer == 1) {
             $allMonsters = $pages->find("template=exercise, teacher=$headTeacher, sort=name");
@@ -263,7 +267,7 @@
             $out .= '</div>';
             $out .= '<div class="well trainingBoard" ng-hide="waitForStart">';
             if ($monster->type->name == 'image-map') {
-              $out .= '<div class=""><img src="'.$monster->imageMap->first()->url.'" max-width="400" alt="Image" /></div>';
+              $out .= '<div class=""><img src="'.$monster->imageMap->url.'" max-width="400" alt="Image" /></div>';
             }
             if ($monster->type->name == 'jumble') {
               $out .= '<span class="pull-right glyphicon glyphicon-question-sign" data-toggle="tooltip" data-html="true" title="'.__("Click on the words to build a correct sentence. If you make a mistake, use the 'Try again' button. If you're wrong, the correct answer will be shown and you just have to copy the correction.<br />See documentation for more information.").'"></span>';
