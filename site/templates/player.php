954bd181a162efdbe08a77922a58b6e470de6317
Fred Lieutaud <flieutaud@posteo.net>
2019-09-02 11:31:06 +0200

    Fix empty history bug Remove inactivity checking for players having a team

--- site/templates/player.php
+++ site/templates/player.php
@@ -31,13 +31,17 @@
   } else {
     $hkCount = '<span class="label label-danger">Private!</span>';
   }
-  // Get last activity # of days
-  $lastEvent = lastEvent($playerPage);
-  $lastActivityCount = daysFromToday($lastEvent);
-  if ($lastActivityCount > 20 && $lastActivityCount < 30) {
-    $helpAlert = true;
-    $helpTitle = __("Watch out for inactivity !");
-    $helpMessage = '<h4>'.__("You will lose all your GC if you don't do anything (UT, fight, donation, ...) before 30 days of inactivity !").'</h4>'; 
+  // Get last activity # of days (for no-team players)
+  if ($playerPage->team->name == 'no-team') {
+    $lastEvent = lastEvent($playerPage);
+    $lastActivityCount = daysFromToday($lastEvent);
+    if ($lastActivityCount > 20 && $lastActivityCount < 30) {
+      $helpAlert = true;
+      $helpTitle = __("Watch out for inactivity !");
+      $helpMessage = '<h4>'.__("You will lose all your GC if you don't do anything (UT, fight, donation, ...) before 30 days of inactivity !").'</h4>'; 
+    }
+  } else {
+    $lastActivityCount = 0 ;
   }
 
   // helpAlert
